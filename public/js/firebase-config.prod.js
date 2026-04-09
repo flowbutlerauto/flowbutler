808e28fbe7296aadf99b31e69f3885ea1173468e
Yeongdeok-RAON <ydseo@raonglobal.co.kr>
2026-04-09 16:56:31 +0900

    Set up dev environment and fix test app auth/tracking issues

--- public/js/firebase-config.prod.js
+++ public/js/firebase-config.prod.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAly32Slh9AKHtyMRZ_vl2m9lAFt-4Kj6o",
-    authDomain: "flowbutler-dev.firebaseapp.com",
-    projectId: "flowbutler-dev",
-    storageBucket: "flowbutler-dev.firebasestorage.app",
-    messagingSenderId: "648289935946",
-    appId: "1:648289935946:web:59a500d21fd54001d0fcb3"
+    apiKey: "AIzaSyBYQbBznp6VQjy4MdXDK632fLPtMB3LwgM",
+    authDomain: "flowbutler.firebaseapp.com",
+    projectId: "flowbutler",
+    storageBucket: "flowbutler.firebasestorage.app",
+    messagingSenderId: "275549773661",
+    appId: "1:275549773661:web:b62155c0f7d88cb50592b4"
 };
 
 const app = initializeApp(firebaseConfig);
